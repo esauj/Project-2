a6807049cd45a50c15f78c1320e54fb0117e7e48
joshuaesau <joshuaesau@10.211.55.3>
2017-10-14 23:50:21 -0500

    migrated whiteTurn and blackTurn out of main method

--- Project 2/src/othello/Game.java
+++ Project 2/src/othello/Game.java
@@ -9,7 +9,8 @@ package othello;
 import java.util.Scanner;
 
 public class Game extends Board{
-	static boolean up, right, down, left;
+	boolean run = true;
+	static boolean up, upRight, right, downRight, down, downLeft, left, upLeft;
 	boolean movesLeft = true;
 	int blackScore = 0;
 	int whiteScore = 0;
@@ -17,6 +18,7 @@ public class Game extends Board{
 	final String PLAYER_WHITE = "White";
 	String currentPlayer = PLAYER_BLACK;
 	boolean valid;
+	Scanner input = new Scanner(System.in);
 	
 	/**
 	 * @param Specifies what color to pass through overridden constructor
@@ -136,12 +138,132 @@ public class Game extends Board{
 				}*/
 			}
 		}
+		
+		System.out.println("Black Score: " + blackScore + " White Score: " + whiteScore);
+		if(blackScore > whiteScore) {
+			System.out.println("BLACK PLAYER WINS WITH SCORE OF " + blackScore);
+		}
+		else if(whiteScore > blackScore) {
+			System.out.println("WHITE PLAYER WINS WITH SCORE OF " + whiteScore);
+		}
+		else {
+			System.out.println("SOMEHOW, you tied. Is this even possible?");
+		}
+		
+	}
+	
+	private void blackTurn() {
+		System.out.println();
+		System.out.println("Current Player: BLACK");
+		System.out.println("BLACK Player, enter your next move in format \"x y\"");
+		System.out.println("You may also enter p to pass or q to quit the program");
+		
+		try{
+			if(input.hasNextInt() == true) {
+				int x = input.nextInt();
+				int y = input.nextInt();
+				//adjust coordinates from user input to array coordinates 
+				x--;
+				y--;
+				isValidMove(x, y, setPiece(BLACK));
+				if(valid == false) {
+					System.out.println("Invalid move! Piece must be placed in a straight line across opponent's piece.");
+					nextTurn();
+				}
+				else {
+					if(up == true) {
+						setSquare(x, y-1, BLACK);
+					}
+					if(down == true) {
+						setSquare(x, y+1, BLACK);
+					}
+					if(right == true) {
+						setSquare(x+1, y, BLACK);
+					}
+					if(left == true) {
+						setSquare(x-1, y, BLACK);
+					}
+					setSquare(x, y, setPiece(BLACK));
+					drawBoard();
+				}
+			}
+			else if(input.hasNext("q") == true) {
+				run = false;
+			}
+			else if(input.hasNext("p") == true) {
+				System.out.println(input.next() + " selected. Turn passed");
+				nextTurn();
+			}
+			else {
+				System.out.println(input.next() + " is not valid input.");
+				nextTurn();
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Numbers must be between 1 and 8!");
+			nextTurn();
+		}
+		nextTurn();
+	}
+	
+	private void whiteTurn() {
+		System.out.println();
+		System.out.println("Current Player: WHITE");
+		System.out.println("WHITE Player, enter your next move in format \"x y\"");
+		System.out.println("You may also enter p to pass or q to quit the program");
+		
+		try {
+			if(input.hasNextInt() == true) {
+				int x = input.nextInt();
+				int y = input.nextInt();
+				//adjust coordinates from user input to array coordinates
+				x--;
+				y--;
+				isValidMove(x, y, setPiece(WHITE));
+				if(valid == false) {
+					System.out.println("Invalid move! Piece must be placed in a straight line across opponent's piece.");
+					nextTurn();
+				}
+				else {
+					if(up == true) {
+						setSquare(x, y-1, WHITE);
+					}
+					if(down == true) {
+						setSquare(x, y+1, WHITE);
+					}
+					if(right == true) {
+						setSquare(x+1, y, WHITE);
+					}
+					if(left == true) {
+						setSquare(x-1, y, WHITE);
+					}
+					setSquare(x, y, setPiece(WHITE));
+					drawBoard();
+				}
+			}
+			else if(input.hasNext("q") == true) {
+				run = false;
+			}
+			else if(input.hasNext("p") == true) {
+				nextTurn();
+				System.out.println("Turn passed");
+				nextTurn();
+			}
+			else {
+				System.out.println(input.next() + " is not valid input.");
+				nextTurn();
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Numbers must be between 1 and 8!");
+			nextTurn();
+		}
+		nextTurn();
 	}
 	
 	public static void main(String[] args) {
-		boolean run = true;
+		
 		Game othello = new Game(BLANK);
-		Scanner input = new Scanner(System.in);
 		
 		//ensures the board is blank and writes beginning pieces before drawing
 		othello.wipeBoard();
@@ -151,140 +273,29 @@ public class Game extends Board{
 		othello.setSquare(4, 4, othello.setPiece(WHITE));
 		othello.drawBoard();
 
-		while(run == true) {
-			//BLACK player turn
+		while(othello.run == true) {
+
 			if(othello.currentPlayer == othello.PLAYER_BLACK) {
-				System.out.println();
-				System.out.println("Current Player: BLACK");
-				System.out.println("BLACK Player, enter your next move in format \"x y\"");
-				System.out.println("You may also enter p to pass or q to quit the program");
-				
-				try{
-					if(input.hasNextInt() == true) {
-						int x = input.nextInt();
-						int y = input.nextInt();
-						//adjust coordinates from user input to array coordinates 
-						x--;
-						y--;
-						othello.isValidMove(x, y, othello.setPiece(BLACK));
-						if(othello.valid == false) {
-							System.out.println("Invalid move! Piece must be placed in a straight line across opponent's piece.");
-							othello.nextTurn();
-						}
-						else {
-							if(up == true) {
-								othello.setSquare(x, y-1, BLACK);
-							}
-							if(down == true) {
-								othello.setSquare(x, y+1, BLACK);
-							}
-							if(right == true) {
-								othello.setSquare(x+1, y, BLACK);
-							}
-							if(left == true) {
-								othello.setSquare(x-1, y, BLACK);
-							}
-							othello.setSquare(x, y, othello.setPiece(BLACK));
-							othello.drawBoard();
-						}
-					}
-					else if(input.hasNext("q") == true) {
-						run = false;
-					}
-					else if(input.hasNext("p") == true) {
-						System.out.println(input.next() + " selected. Turn passed");
-						othello.nextTurn();
-					}
-					else {
-						System.out.println(input.next() + " is not valid input.");
-						othello.nextTurn();
-					}
-				}
-				catch(ArrayIndexOutOfBoundsException e) {
-					System.out.println("Numbers must be between 1 and 8!");
-					othello.nextTurn();
-				}
-				othello.nextTurn();
+				othello.blackTurn();
 			}
 			
 			//WHITE player turn
 			else if(othello.currentPlayer == othello.PLAYER_WHITE) {
-				System.out.println();
-				System.out.println("Current Player: WHITE");
-				System.out.println("WHITE Player, enter your next move in format \"x y\"");
-				System.out.println("You may also enter p to pass or q to quit the program");
-				
-				try {
-					if(input.hasNextInt() == true) {
-						int x = input.nextInt();
-						int y = input.nextInt();
-						System.out.println(x + " " + y);
-						x--;
-						y--;
-						System.out.println(x + " " + y);
-						othello.isValidMove(x, y, othello.setPiece(WHITE));
-						if(othello.valid == false) {
-							System.out.println("Invalid move! Piece must be placed in a straight line across opponent's piece.");
-							othello.nextTurn();
-						}
-						else {
-							if(up == true) {
-								othello.setSquare(x, y-1, WHITE);
-							}
-							if(down == true) {
-								othello.setSquare(x, y+1, WHITE);
-							}
-							if(right == true) {
-								othello.setSquare(x+1, y, WHITE);
-							}
-							if(left == true) {
-								othello.setSquare(x-1, y, WHITE);
-							}
-							othello.setSquare(x, y, othello.setPiece(WHITE));
-							othello.drawBoard();
-						}
-					}
-					else if(input.hasNext("q") == true) {
-						run = false;
-					}
-					else if(input.hasNext("p") == true) {
-						othello.nextTurn();
-						System.out.println("Turn passed");
-						othello.nextTurn();
-					}
-					else {
-						System.out.println(input.next() + " is not valid input.");
-						othello.nextTurn();
-					}
-				}
-				catch(ArrayIndexOutOfBoundsException e) {
-					System.out.println("Numbers must be between 1 and 8!");
-					othello.nextTurn();
-				}
-				othello.nextTurn();
+				othello.whiteTurn();
 			}
 			
 			//Runs boardCheck() following each successful move to ensure that more moves exist
 			othello.boardCheck();
 			if(othello.movesLeft == false) {
 				//If no moves remain, game scores itself and prints results
-				run = false;
+				othello.run = false;
 				othello.scoreGame();
-				System.out.println("Black Score: " + othello.blackScore + " White Score: " + othello.whiteScore);
-				if(othello.blackScore > othello.whiteScore) {
-					System.out.println("BLACK PLAYER WINS WITH SCORE OF " + othello.blackScore);
-				}
-				else if(othello.whiteScore > othello.blackScore) {
-					System.out.println("WHITE PLAYER WINS WITH SCORE OF " + othello.whiteScore);
-				}
-				else {
-					System.out.println("SOMEHOW, you tied. Is this even possible?");
-				}
+				
 			}
 		}
 		
 		System.out.println("Goodbye! Thanks for playing!");
-		input.close();
+		othello.input.close();
 		System.exit(0);
 	}	
 }
